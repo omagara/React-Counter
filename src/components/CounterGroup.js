f6d291b41d2315fa9b1afd48f29dca4120b22f6a
omagara <omagara@oocl.com>
2021-08-16 20:53:05 +0800

    [React-Counter][Ramon]feat: implemented generateCounterList in CounterGroup.js

---
+++ src/components/CounterGroup.js
@@ -0,0 +1,21 @@
+import Counter from "./Counter";
+
+function CounterGroup(props){
+
+    function generateCounterList(){
+        return new Array(props.counterSize).fill(Date.now())
+    }
+    const counterList = generateCounterList();
+    return (
+    <div>
+        {counterList.map((item, index)=> (
+            <Counter key = {item + index}></Counter>
+        ))}
+
+    </div>
+);
+
+}
+
+
+export default CounterGroup;
