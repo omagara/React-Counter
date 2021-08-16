68f271f1977111d5c048db157303f44c0765fb69
omagara <omagara@oocl.com>
2021-08-16 20:53:59 +0800

    [React-Counter][Ramon]feat: implemented handleChangeSize in CounterSizeGenerator.js

---
+++ src/components/CounterSizeGenerator.js
@@ -0,0 +1,21 @@
+import { useState } from "react";
+
+function CounterSizeGenerator(props) {
+    const [size, setSize] = useState(0);
+
+    function handleChangeSize(event){
+        setSize(parseInt(event.target.value));
+
+        props.updateCounterSize(parseInt(event.target.value))
+    }
+
+    return (
+        <div>
+            <span>Size:</span>
+            <input type="number" min="0" value={size} onChange={handleChangeSize}></input>
+        </div>
+    );
+}
+
+
+export default CounterSizeGenerator;
